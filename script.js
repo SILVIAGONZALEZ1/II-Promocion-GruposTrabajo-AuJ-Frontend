1e3774dd74c5ff036ae6f19cc79a6339c10928aa
Silvia Gonzalez <devsoftwaregonzalez@gmail.com>
2024-08-26 00:54:27 -0300

    se carga nueva imagen, se realizan mejoras css

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 document.getElementById('contactForm').addEventListener('submit', function(event) {
-    event.preventDefault(); // Evita el envío del formulario por defecto
-    
-    // Aquí puedes añadir la validación personalizada si es necesario
-    
-    // Si todo está bien, se puede enviar el formulario
-    alert('Formulario enviado correctamente!');
-    // O puedes manejar el envío del formulario con AJAX, por ejemplo.
-  });
+  event.preventDefault(); // Evita el envío del formulario por defecto
+
+  // Aquí puedes añadir la validación personalizada si es necesario
+
+  // Si todo está bien, se puede enviar el formulario
+  alert('Formulario enviado correctamente!');
   
+  // Limpiar el formulario
+  this.reset(); // Resetea todos los campos del formulario a su estado inicial
+});
+
 
 
